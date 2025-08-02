13babeef72efed75f207bf305d7d439c1d990cec
satish <sk95satish@gmail.com>
2025-08-02 16:27:10 +0530

    Validator change in API Gateway

--- API-GATEWAY/src/main/java/com/gateway/filter/Validator.java
+++ API-GATEWAY/src/main/java/com/gateway/filter/Validator.java
@@ -2,6 +2,7 @@ package com.gateway.filter;
 
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
+import org.springframework.util.AntPathMatcher;
 
 import java.util.List;
 import java.util.function.Predicate;
@@ -9,12 +10,21 @@ import java.util.function.Predicate;
 @Component
 public class Validator {
 
+    private final AntPathMatcher pathMatcher = new AntPathMatcher();
+
     public static final List<String> endpoints = List.of(
             "/register-user",
             "/generate-token",
             "/validate-token/{token}"
     );
+//
+//    Predicate<ServerHttpRequest> predicate = serverHttpRequest -> endpoints.stream()
+//            .noneMatch(uri -> serverHttpRequest.getURI().getPath().contains(uri));
 
-    Predicate<ServerHttpRequest> predicate = serverHttpRequest -> endpoints.stream()
-            .noneMatch(uri -> serverHttpRequest.getURI().getPath().contains(uri));
+
+    public Predicate<ServerHttpRequest> predicate = serverHttpRequest -> {
+        String requestPath = serverHttpRequest.getURI().getPath();
+        return endpoints.stream()
+                .noneMatch(uri -> pathMatcher.match(uri, requestPath));
+    };
 }
